4797c13ba34041d7baf453456be0ea2af5b270d1
Sujith <37608689+Sujith46@users.noreply.github.com>
2023-06-07 22:53:30 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { motion } from "framer-motion";
+import "./styles.css";
+
+const container = {
+  show: {
+    transition: {
+      delayChildren: 0.2,
+      staggerChildren: 0.1
+    }
+  }
+};
+
+const item = {
+  hidden: { y: 100 },
+  show: {
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 1
+    }
+  }
+};
+
+type AnimatedSpanType = {
+  text: string[];
+};
+
+export default function App() {
+  const headerOne = "Hello";
+  const headerTwo = "World 😍";
+
+  const AnimatedTextSpan = ({ text }: AnimatedSpanType) => (
+    <motion.span
+      className="span-container"
+      variants={container}
+      initial="hidden"
+      animate="show"
+    >
+      {[...text].map((letters, index) => (
+        <motion.span key={index} className="span-letters" variants={item}>
+          {letters}
+        </motion.span>
+      ))}
+    </motion.span>
+  );
+  return (
+    <motion.div variants={container} className="App">
+      <AnimatedTextSpan text={[...headerOne]} />
+      <AnimatedTextSpan text={[...headerTwo]} />
+    </motion.div>
+  );
+}
